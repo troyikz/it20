16cedd9c33ec286232c226ff371a51c81551b3f4
Troyikz <rojotroy40@gmail.com>
2024-09-23 12:42:02 +0800

    initial commit

---
+++ src/it20_rojo/IT20_Rojo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it20_rojo;
+
+/**
+ *
+ * @author Troy M. Rojo
+ */
+public class IT20_Rojo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("My Repository");
+    }
+    
+}
