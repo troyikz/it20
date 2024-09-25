a91ff0f7b9be9b1e7bea85772dc8e80f4d3ec9e0
Troyikz <rojotroy40@gmail.com>
2024-09-25 12:02:30 +0800

    initial commit

---
+++ src/Secondpackage/Forloop.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Secondpackage;
+
+/**
+ *
+ * @author Erick James Rubin
+ */
+public class Forloop {
+
+    public static void main(String[] args) {
+        // Declare and initialize a character array
+        char[] letters = {'A', 'B', 'C', 'D'};
+
+        // Use a for loop to print each element
+        for (int i = 0; i < letters.length; i++) {
+            System.out.println(letters[i]);
+        }
+    }
+}
+
+    
+
